84a404503d82d712a56827ac1efe8f3ccf1108ca
Andrea Maggino <andreamaggino99@gmail.com>
2020-03-28 23:09:29 +0000

    Fixing Heroku connection

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -113,10 +112,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-#AUTHENTICATION_BACKENDS = [
+# AUTHENTICATION_BACKENDS = [
 #    'django.contrib.auth.backends.ModelBackend',
 #    'accounts.backends.CaseInsensitiveAuth'
-#]
+# ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
